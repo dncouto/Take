9e5c491ce60d189af9f10daa105ec896b2cee1f7
daniel couto <dncouto@gmail.com>
2020-04-26 19:54:28 -0300

    Conclusão teste MessageController

--- ServerChatTest/TestMessageController.cs
+++ ServerChatTest/TestMessageController.cs
@@ -8,6 +8,7 @@ using ServerChat.Service;
 using ServerChat.Service.Intf;
 using System.Collections.Generic;
 using System.Net.WebSockets;
+using System.Threading.Tasks;
 using Assert = NUnit.Framework.Assert;
 
 namespace ServerChatTest
@@ -17,20 +18,19 @@ namespace ServerChatTest
     {
         Mock<IConnectionManagerService> connectionManagerService;
         MessageController messageController;
+        Mock<WebSocket> mockWebSoquet;
 
         [SetUp]
         public void Setup()
         {
             connectionManagerService = new Mock<IConnectionManagerService>();
             messageController = new MessageController(new MessageProducerService(), connectionManagerService.Object);
+            mockWebSoquet = new Mock<WebSocket>();
         }
 
         [Test]
         public void GetCommands()
         {
-            // Arrange
-            //mockMessageProducerService.Setup(x => x.GetAllCommands()).Returns(new List<string> { "a", "b", "c" });
-
             // Act
             var actionResult = messageController.GetCommands();
             
@@ -73,12 +73,82 @@ namespace ServerChatTest
             AssertSendErrorResponseMessage(actionResult, "O usuário que enviou a mensagem é obrigatório e não foi informado!");
         }
 
+        [Test]
+        public void SendMessageNull()
+        {
+            // Arrange
+            MessageDTO message = MessageDTO.Build(false, "origin", null, "");
+
+            // Act
+            var actionResult = messageController.Send(message);
+
+            // Assert
+            AssertSendErrorResponseMessage(actionResult, "Mensagem não pode ser vazia!");
+        }
+
+        [Test]
+        public void SendPrivateUserDestinyNull()
+        {
+            // Arrange
+            MessageDTO message = MessageDTO.Build(true, "origin", null, "xxx");
+            
+            // Act
+            var actionResult = messageController.Send(message);
+
+            // Assert
+            AssertSendErrorResponseMessage(actionResult, "Para mensagens privada o destinatário é OBRIGATÓRIO!");
+        }
+
+        [Test]
+        public void SendSuccess()
+        {
+            // Arrange TODOS
+            MessageDTO message = MessageDTO.Build(false, "origin", "recipient", "xxx");
+            connectionManagerService
+                .Setup(x => x.GetSocketByNickName(message.To))
+                .Returns(mockWebSoquet.Object);
+
+            connectionManagerService
+                .Setup(x => x.SendMessageEveryOne(message.Message))
+                .Returns(Task.FromResult(""));
+
+            // Act TODOS
+            var actionResult = messageController.Send(message);
+
+            // Assert TODOS
+            AssertSendSuccessResponseMessage(actionResult, "SUCCESS");
+
+
+            // Arrange PRIVADO
+            message = MessageDTO.Build(true, "origin", "recipient", "xxx");
+            connectionManagerService
+                .Setup(x => x.SendMessagePrivate(message.From, message.Message))
+                .Returns(Task.FromResult(""));
+
+            // Act PRIVADO
+            actionResult = messageController.Send(message);
+
+            // Assert PRIVADO
+            AssertSendSuccessResponseMessage(actionResult, "SUCCESS");
+        }
+
         private void AssertSendErrorResponseMessage(dynamic actionResult, string corretMsg)
         {
             Assert.IsNotNull(actionResult);
             Assert.IsInstanceOf(typeof(ObjectResult), actionResult.Result);
             ObjectResult objectResult = actionResult.Result as ObjectResult;
             Assert.AreEqual(objectResult.StatusCode, 400);
+            Assert.IsInstanceOf(typeof(string), objectResult.Value);
+            string value = objectResult.Value as string;
+            Assert.AreEqual(value, corretMsg);
+        }
+
+        private void AssertSendSuccessResponseMessage(dynamic actionResult, string corretMsg)
+        {
+            Assert.IsNotNull(actionResult);
+            Assert.IsInstanceOf(typeof(ObjectResult), actionResult.Result);
+            ObjectResult objectResult = actionResult.Result as ObjectResult;
+            Assert.AreEqual(objectResult.StatusCode, 200);
             Assert.IsInstanceOf(typeof(string), objectResult.Value);
             string value = objectResult.Value as string;
             Assert.AreEqual(value, corretMsg);
